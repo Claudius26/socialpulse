11fb9ae367edffcac9df2f53f6cb45fedd2c875c
Claudius26 <osomzed@gmail.com>
2025-11-02 09:38:32 +0100

    added layout properly

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Sidebar from "../components/Sidebar";
@@ -5,12 +6,16 @@ import { Outlet } from "react-router";
 import { ToastContainer, Slide } from "react-toastify";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../features/auth/authSlice";
+import { Menu } from "lucide-react";
 
 function Layout() {
   const user = useSelector(selectCurrentUser);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  // Not logged in layout
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
+
   if (!user) {
+   
     return (
       <div className="flex flex-col min-h-screen bg-gray-50">
         <NavBar />
@@ -28,24 +33,37 @@ function Layout() {
           draggable
           transition={Slide}
           theme="dark"
-          className="z-[9999]"
         />
       </div>
     );
   }
 
-  // Logged in layout
+  
   return (
-    <div className="flex min-h-screen bg-gradient-to-br from-blue-50 via-blue-100 to-gray-100">
-      {/* Sidebar */}
-      <Sidebar isOpen={true} />
+    <div className="flex min-h-screen bg-gradient-to-br from-blue-50 via-blue-100 to-gray-100 relative overflow-hidden">
+      {/* Mobile Menu Button */}
+      <button
+        className="absolute top-4 left-4 z-50 bg-blue-600 text-white p-2 rounded-md md:hidden"
+        onClick={toggleSidebar}
+      >
+        <Menu size={22} />
+      </button>
 
-      {/* Main content area */}
-      <div className="flex-1 ml-56 md:ml-60 lg:ml-64 p-6 overflow-y-auto">
-        <Outlet />
+      
+      <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+
+      
+      <div
+        className={`flex-1 transition-all duration-300 md:ml-60 lg:ml-64 ${
+          sidebarOpen ? "blur-sm md:blur-0" : ""
+        }`}
+      >
+        <div className="p-4 sm:p-6 md:p-8 overflow-x-hidden">
+          <Outlet />
+        </div>
       </div>
 
-      {/* Toasts */}
+      
       <ToastContainer
         position="top-right"
         autoClose={3000}
@@ -56,7 +74,6 @@ function Layout() {
         draggable
         transition={Slide}
         theme="dark"
-        className="z-[9999]"
       />
     </div>
   );
