caa66c85de4eec5ba3896fc2b260db92ecb4fde7
Claudius26 <osomzed@gmail.com>
2025-11-02 10:11:31 +0100

    added it

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,21 +1,15 @@
-import { useState } from "react";
-import NavBar from "../components/Navbar";
-import Footer from "../components/Footer";
-import Sidebar from "../components/Sidebar";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router";
 import { ToastContainer, Slide } from "react-toastify";
-import { useSelector } from "react-redux";
+import Sidebar from "../components/Sidebar";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
 import { selectCurrentUser } from "../features/auth/authSlice";
-import { Menu } from "lucide-react";
 
 function Layout() {
   const user = useSelector(selectCurrentUser);
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
 
   if (!user) {
-   
     return (
       <div className="flex flex-col min-h-screen bg-gray-50">
         <NavBar />
@@ -38,32 +32,14 @@ function Layout() {
     );
   }
 
-  
   return (
-    <div className="flex min-h-screen bg-gradient-to-br from-blue-50 via-blue-100 to-gray-100 relative overflow-hidden">
-      {/* Mobile Menu Button */}
-      <button
-        className="absolute top-4 left-4 z-50 bg-blue-600 text-white p-2 rounded-md md:hidden"
-        onClick={toggleSidebar}
-      >
-        <Menu size={22} />
-      </button>
+    <div className="flex min-h-screen bg-gradient-to-br from-blue-50 via-blue-100 to-gray-100 overflow-hidden">
+      <Sidebar />
 
-      
-      <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
-
-      
-      <div
-        className={`flex-1 transition-all duration-300 md:ml-60 lg:ml-64 ${
-          sidebarOpen ? "blur-sm md:blur-0" : ""
-        }`}
-      >
-        <div className="p-4 sm:p-6 md:p-8 overflow-x-hidden">
-          <Outlet />
-        </div>
+      <div className="flex-1 ml-56 md:ml-60 lg:ml-64 p-4 sm:p-6 md:p-8 overflow-x-hidden">
+        <Outlet />
       </div>
 
-      
       <ToastContainer
         position="top-right"
         autoClose={3000}
